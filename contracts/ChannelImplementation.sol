7a4d07cf270911fdea0160fdda0265aa6edcecf7
Jaro <jaro@nous.lt>
2019-04-12 12:40:08 +0300

    Changing Promisses into channels

---
+++ contracts/ChannelImplementation.sol
@@ -0,0 +1,250 @@
+pragma solidity ^0.5.0;
+
+import { ECDSA } from "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
+import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import { DEXProxy } from "./DEXProxy.sol";
+import { FundsRecovery } from "./FundsRecovery.sol";
+
+interface MystDEX {
+    function initialise(address _dexOwner, address _token, uint256 _rate) external;
+}
+
+/*
+    Ledger Channel between user and hub.
+    This is channel implementation into which mini proxies will point to.
+*/
+contract ChannelImplementation is FundsRecovery {
+    using ECDSA for bytes32;
+    using SafeMath for uint256;
+
+    string constant REBALANCE_PREFIX = "Rebalancing channel balances:";
+    string constant EXIT_PREFIX = "Exit request:";
+
+    event IdentityBalanceUpdated(uint256 balance);
+    event ChannelStateUpdated(uint256 identityBalance, uint256 hubBalance, uint256 sequence);
+    event FundsWithdrawn(address indexed party, address indexed beneficiary, uint256 amount);
+    event ExitRequested(address party, uint256 timeout);
+
+    struct Withdrawal {
+        address party;
+        address beneficiary;
+        uint256 timeout;
+    }
+
+    IERC20 public token;             // NOTE: token can be actually constant or be received from external config
+    address public dex;
+
+    address public identityHash;     // sha3(identityPubKey)[:20]
+    address public hubId;            // TODO get hubOperator here (from hubId contract)
+    uint256 public identityBalance;
+    uint256 public hubBalance;
+    uint256 public lastSequence;
+    uint256 public challengePeriod;  // Time in seconds
+    Withdrawal public pendingWithdrawal; 
+
+    // Only usefull in tests
+    // constructor (address _token, address _DEXImplementation, address _DEXOwner, uint256 _rate) public {
+    //     require(_token != address(0));
+    //     require(_DEXImplementation != address(0));
+    //     require(_DEXOwner != address(0));
+
+    //     // Deploy MystDex proxy and set default target to `_DEXImplementation`
+    //     dex = address(new DEXProxy(_DEXImplementation, _DEXOwner));
+    //     MystDEX(dex).initialise(_DEXOwner, _token, _rate);
+    // }
+
+    // Fallback function - redirect ethers topup into DEX
+    function () external payable {
+        (bool success, bytes memory data) = address(dex).call.value(msg.value)(msg.data);
+        require(success, "Tx was rejected by DEX");
+    }
+
+    // Because of proxy pattern this function is used insted of constructor.
+    // Have to be called right after proxy deployment.
+    function initialize(address _token, address _dex, address _identityHash, address _hubId, uint256 _challengePeriod) public {
+        require(!isInitialized(), "Is already initialized");
+        require(_identityHash != address(0), "Identity can't be zero");
+        require(_hubId != address(0), "HubID can't be zero");
+        require(_token != address(0), "Token can't be deployd into zero address");
+        
+        token = IERC20(_token);
+        dex = _dex;
+        identityHash = _identityHash;
+        hubId = _hubId;
+        challengePeriod = _challengePeriod;
+
+        updateIdentityBalance(); // in case there were token sent before contract deplyment
+    }
+
+    function isInitialized() public view returns (bool) {
+        return identityHash != address(0);
+    }
+
+    // Topup via preliminary allowance. The only way to topup for hub.
+    function deposit(address _party, uint256 _amount) public {
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        if (_party == hubId) {
+            hubBalance += _amount;
+        } else {
+            identityBalance += _amount;
+        }
+
+        require(
+            identityBalance.add(hubBalance) == token.balanceOf(address(this)),
+            "sum balances must be equal to amount of locked tokens"
+        );
+    }
+
+    // Will check if there was no tokens send into channel, and if founds some, add them into identity balance
+    function updateIdentityBalance() public {
+        identityBalance = token.balanceOf(address(this)).sub(hubBalance);
+        emit IdentityBalanceUpdated(identityBalance);
+    }
+
+    // Alternative way to work with Identity balances TODO: add it in separate PR
+    // function identityBalance() public view returns (uint256) {
+    //     return token.balanceOf(address(this)).sub(hubBalance);
+    // }
+
+    // Update/rebalance channel
+    function update(uint256 _identityBalance, uint256 _hubBalance, uint256 _sequence, bytes memory _identitySig, bytes memory _hubSig) public {
+        require(_sequence > lastSequence, "provided sequence must be bigger than already seen");
+        require(_identityBalance.add(_hubBalance) == token.balanceOf(address(this)), "sum of balances must be equal to amount of locked tokens");
+
+        bytes32 _hash = keccak256(abi.encodePacked(REBALANCE_PREFIX, _identityBalance, _hubBalance, _sequence));
+
+        address _recoveredIdentity = _hash.recover(_identitySig);
+        require(_recoveredIdentity == identityHash, "wrong identity signature");
+
+        address _recoveredHub = _hash.recover(_hubSig);
+        require(_recoveredHub == hubId, "wrong hub signature");
+
+        // Update channel state
+        identityBalance = _identityBalance;
+        hubBalance = _hubBalance;
+        lastSequence = _sequence;
+
+        emit ChannelStateUpdated(identityBalance, hubBalance, lastSequence);
+    }
+
+    // Fast withdraw request (with rebalancing)
+    function updateAndWithdraw(uint256 _identityBalance, uint256 _hubBalance, 
+                               uint256 _identityWithdraw, uint256 _hubWithdraw,
+                               uint256 _sequence, uint256 _timeout, bytes memory _identitySig, bytes memory _hubSig) public {
+        require(now <= _timeout, "fast withdraw signatures timeout");
+        require(_sequence > lastSequence, "provided sequence must be bigger than already seen");
+        require(_identityBalance.add(_hubBalance).add(_identityWithdraw).add(_hubWithdraw) == token.balanceOf(address(this)), "sum of balances must be equal to amount of locked tokens");
+
+        bytes32 _hash = keccak256(abi.encodePacked(REBALANCE_PREFIX, _identityBalance, _hubBalance, _identityWithdraw, _hubWithdraw, _sequence, _timeout));
+
+        address _recoveredIdentity = _hash.recover(_identitySig);
+        require(_recoveredIdentity == identityHash, "wrong identity signature");
+
+        address _recoveredHub = _hash.recover(_hubSig);
+        require(_recoveredHub == hubId, "wrong hub signature");
+
+        // Withdraw funds
+        if (_identityWithdraw > 0) {
+            token.transfer(identityHash, _identityWithdraw); // TODO point where to withdraw
+            emit FundsWithdrawn(identityHash, identityHash, _identityWithdraw);
+        }
+
+        if (_hubWithdraw > 0) {
+            token.transfer(hubId, _hubWithdraw);
+            emit FundsWithdrawn(hubId, hubId, _hubWithdraw);
+        }
+
+        require(_identityBalance.add(_hubBalance) == token.balanceOf(address(this)), "sum of balances must be equal to amount of locked tokens");
+
+        // Update channel state
+        identityBalance = _identityBalance;
+        hubBalance = _hubBalance;
+        lastSequence = _sequence;
+
+        emit ChannelStateUpdated(identityBalance, hubBalance, lastSequence);
+    }
+
+    /*
+        Exit (withdrawal with delay for challanges/channel state updates)
+    */
+
+    // TODO add bounty for challenge.
+    // Start emergency withdrawal of all funds --> usually when another party is not collaborating for `updateAndWithdraw`
+    function exitRequest(address _party, address _beneficiary, bytes memory _signature) public {
+        require(_party == identityHash || _party == hubId, "party should be either identity either hub");
+        require(_beneficiary != address(0), "tokens can't be burned");
+        // require(channel.status == ChannelStatus.Open, "channel must be open");
+
+        address _recoveredParty = keccak256(abi.encodePacked(EXIT_PREFIX, _party, _beneficiary)).recover(_signature);
+        require(_recoveredParty == _party, "wrong signature");
+
+        // channel.status = ChannelStatus.ChannelDispute;
+        uint256 _timeout = now.add(challengePeriod);
+        pendingWithdrawal = Withdrawal(_party, _beneficiary,  _timeout);
+
+        emit ExitRequested(_party, _timeout);
+    }
+
+    // Emergency withdrawal when there are not signature from another party
+    function updateAndExit(address _party, address _beneficiary, bytes memory _signature,
+                           uint256 _identityBalance, uint256 _hubBalance, 
+                           uint256 _sequence, bytes memory _identitySig, bytes memory _hubSig) public {
+        require(_party == identityHash || _party == hubId, "party should be either identity either hub");
+        
+        update(_identityBalance, _hubBalance, _sequence, _identitySig, _hubSig); // Update channel into last known state
+        exitRequest(_party, _beneficiary, _signature);
+    }
+
+    function finalizeExit() public {
+        require(pendingWithdrawal.party != address(0), "there should be pending withdrawal");
+        require(pendingWithdrawal.timeout != 0 && now > pendingWithdrawal.timeout, "timeout should be passed");
+
+        uint256 _amount;
+
+        if (pendingWithdrawal.party == identityHash) {
+            _amount = identityBalance;
+            identityBalance = 0;
+        } else {
+            _amount = hubBalance;
+            hubBalance = 0;
+        }
+        
+        token.transfer(pendingWithdrawal.beneficiary, _amount);
+        pendingWithdrawal = Withdrawal(address(0), address(0), 0);
+
+        emit FundsWithdrawn(pendingWithdrawal.party, pendingWithdrawal.beneficiary, _amount);
+    }
+
+    /*
+        Helpers
+    */
+    string constant PERIOD_CHANGE_PREFIX = "Challenge period change request:";
+    event ChallengePeriodChanged(uint256 challengePeriod);
+    function updateChallengePeriod(uint256 _newChallengePediod, bytes memory _identitySig, bytes memory _hubSig) public {
+        bytes32 _hash = keccak256(abi.encodePacked(PERIOD_CHANGE_PREFIX, _newChallengePediod));
+
+        address _recoveredIdentity = _hash.recover(_identitySig);
+        require(_recoveredIdentity == identityHash, "wrong identity signature");
+
+        address _recoveredHub = _hash.recover(_hubSig);
+        require(_recoveredHub == hubId, "wrong hub signature");    
+    
+        challengePeriod = _newChallengePediod;
+
+        emit ChallengePeriodChanged(_newChallengePediod);
+    }
+
+    // Setting new destination of funds recovery.
+    string constant FUNDS_DESTINATION_PREFIX = "Set funds destination:";
+    function setFundsDestinationByCheque(address payable _newDestination, bytes memory _signature) public {
+        require(_newDestination != address(0));
+
+        address _signer = keccak256(abi.encodePacked(FUNDS_DESTINATION_PREFIX, _newDestination)).recover(_signature);
+        require(_signer == identityHash, "Have to be signed by proper identity");
+
+        emit DestinationChanged(fundsDestination, _newDestination);
+        fundsDestination = _newDestination;
+    }
+}
--- contracts/IdentityRegistry.sol
+++ contracts/IdentityRegistry.sol
@@ -4,7 +4,7 @@ import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import { FundsRecovery } from "./FundsRecovery.sol";
-import { IdentityImplementation } from "./IdentityImplementation.sol";
+import { ChannelImplementation } from "./ChannelImplementation.sol";
 
 
 contract IdentityRegistry is Ownable, FundsRecovery {
@@ -12,19 +12,23 @@ contract IdentityRegistry is Ownable, FundsRecovery {
 
     string constant REGISTER_PREFIX="Register prefix:";
     IERC20 public token;
+    address public dex;
     uint256 public registrationFee;
-    address identityContractImplementation;
+    address channelContractImplementation;
     
     event Registered(address indexed identityHash);
 
-    constructor(address _tokenAddress, uint256 _regFee, address _implementation) public {
+    constructor (address _tokenAddress, address _dexAddress, uint256 _regFee, address _implementation) public {
         registrationFee = _regFee;
 
         require(_tokenAddress != address(0));
         token = IERC20(_tokenAddress);
+
+        require(_dexAddress != address(0));
+        dex = _dexAddress;
         
         require(_implementation != address(0));
-        identityContractImplementation = _implementation;
+        channelContractImplementation = _implementation;
     }
 
     // Reject any ethers send to this smart-contract
@@ -32,30 +36,7 @@ contract IdentityRegistry is Ownable, FundsRecovery {
         revert("Rejecting tx with ethers sent");
     }
 
-    // Alternative implementation when pubKeys are stored in blockchain
-    // function registerIdentity(bytes32 _pubKeyPart1, bytes32 _pubKeyPart2, bytes memory _signature) public {
-    //     address _identity = keccak256(abi.encodePacked(REGISTER_PREFIX, _pubKeyPart1, _pubKeyPart2)).recover(_signature);
-    //     address _identityFromPubKey = address(uint(keccak256(abi.encodePacked(_pubKeyPart1, _pubKeyPart2))));
-
-    //     require(_identity != address(0));
-    //     require(_identityFromPubKey == _identity);
-    //     require(!isRegistered(_identity));
-
-    //     registeredIdentities[_identity] = PublicKey({ part1: _pubKeyPart1, part2: _pubKeyPart2});
-
-    //     if (registrationFee > 0) {
-    //         token.operatorSend(msg.sender, address(this), registrationFee, "", "");
-    //     }
-
-    //     // Deploy identity contract (mini proxy which is pointing to implementation)
-    //     // require(deployMiniProxy(uint256(_identity)) == getIdentityContractAddress(_identity), "Wrong identity contract address");
-    //     IdentityImplementation _identityContract = IdentityImplementation(deployMiniProxy(uint256(_identity)));
-    //     _identityContract.initialize(address(token), _identity);
-
-    //     emit Registered(_identity);
-    // }
-
-    function registerIdentity(address _identityHash) public {
+    function registerIdentity(address _identityHash, address _hubId) public {
         require(_identityHash != address(0));
         require(!isRegistered(_identityHash));
 
@@ -63,11 +44,9 @@ contract IdentityRegistry is Ownable, FundsRecovery {
             token.transferFrom(msg.sender, address(this), registrationFee);
         }
 
-        // Deploy identity contract (mini proxy which is pointing to implementation)
-        // Possible recheck:
-        // require(deployMiniProxy(uint256(_identityHash)) == getIdentityContractAddress(_identity), "Wrong identity contract address");
-        IdentityImplementation _identityContract = IdentityImplementation(deployMiniProxy(uint256(_identityHash)));
-        _identityContract.initialize(address(token), _identityHash);
+        // Deploy channel contract for given identity (mini proxy which is pointing to implementation)
+        ChannelImplementation _channel = ChannelImplementation(deployMiniProxy(uint256(_identityHash)));
+        _channel.initialize(address(token), dex, _identityHash, _hubId, 259200); // 259 200 = 3 days challenge period
 
         emit Registered(_identityHash);
     }
@@ -86,14 +65,14 @@ contract IdentityRegistry is Ownable, FundsRecovery {
         return _addr;
     }
 
-    // NOTE: in final implementation this function will return static code (with `identityContractImplementation` address hardcoded there).
+    // NOTE: in final implementation this function will return static code (with `channelContractImplementation` address hardcoded there).
     // We're using this way now for easier testing.
     function getProxyCode() public view returns (bytes memory) {
         // `_code` is EIP 1167 - Minimal Proxy Contract
         // more information: https://eips.ethereum.org/EIPS/eip-1167
         bytes memory _code = hex"3d602d80600a3d3981f3363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe5af43d82803e903d91602b57fd5bf3";
 
-        bytes20 _targetBytes = bytes20(identityContractImplementation);
+        bytes20 _targetBytes = bytes20(channelContractImplementation);
         for (uint8 i = 0; i < 20; i++) {
             _code[20 + i] = _targetBytes[i];
         }
@@ -101,7 +80,7 @@ contract IdentityRegistry is Ownable, FundsRecovery {
         return _code;
     }
 
-    function getIdentityContractAddress(address _identityHash) public view returns (address) {
+    function getIdentityChannelAddress(address _identityHash) public view returns (address) {
         return address(uint256(keccak256(abi.encodePacked(
             bytes1(0xff),
             address(this),
@@ -111,7 +90,7 @@ contract IdentityRegistry is Ownable, FundsRecovery {
     }
 
     function isRegistered(address _identityHash) public view returns (bool) {
-        address _addr = getIdentityContractAddress(_identityHash);
+        address _addr = getIdentityChannelAddress(_identityHash);
         uint _codeLength;
 
         assembly {
@@ -120,16 +99,6 @@ contract IdentityRegistry is Ownable, FundsRecovery {
 
         return _codeLength != 0;
     }
-
-    // Alternative implementation when public keys are stored in blockchain
-    // function isRegistered(address _identity) public view returns (bool) {
-    //     return registeredIdentities[_identity].part1 != 0x0;
-    // }
-
-    // function getPublicKey(address _identity) public view returns (bytes32, bytes32) {
-    //     PublicKey storage _pubKey = registeredIdentities[_identity];
-    //     return (_pubKey.part1, _pubKey.part2);
-    // }
 
     function changeRegistrationFee(uint256 _newFee) public onlyOwner {
         registrationFee = _newFee;
