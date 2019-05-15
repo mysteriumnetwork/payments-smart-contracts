aef922247993cb5159f3fbd7ba3e5d951fd19392
Jaro <jaro@nous.lt>
2019-05-15 13:04:00 +0300

    Tests for paid registration

--- test/paidRegistration.js
+++ test/paidRegistration.js
@@ -2,21 +2,25 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-const IdentityRegistry = artifacts.require("IdentityRegistry")
-const IdentityImplementation = artifacts.require("IdentityImplementation")
+const topUpTokens = require('./utils/index.js').topUpTokens
+
+const Registry = artifacts.require("Registry")
+const ChannelImplementation = artifacts.require("ChannelImplementation")
+const AccountantImplementation = artifacts.require("AccountantImplementation")
 const MystToken = artifacts.require("MystToken")
 const MystDex = artifacts.require("MystDEX")
 
 const OneEther = web3.utils.toWei('1', 'ether')
 const identityHash = '0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
 
-contract('Paid identity registry', ([owner, ...otherAccounts]) => {
-    let token, identityImplementation, dex, registry
+contract('Paid identity registry', ([txMaker, accountantOperator, fundsDestination, ...otherAccounts]) => {
+    let token, channelImplementation, accountantId, dex, registry
     before(async () => {
         token = await MystToken.new()
         dex = await MystDex.new()
-        identityImplementation = await IdentityImplementation.new(token.address, dex.address, owner, OneEther)
-        registry = await IdentityRegistry.new(token.address, 0, identityImplementation.address)
+        const accountantImplementation = await AccountantImplementation.new()
+        channelImplementation = await ChannelImplementation.new()
+        registry = await Registry.new(token.address, dex.address, channelImplementation.address, accountantImplementation.address, 0, 0)
     })
 
     it('should have zero registration fee', async () => {
@@ -39,9 +43,20 @@ contract('Paid identity registry', ([owner, ...otherAccounts]) => {
         expect(await registry.isRegistered(identityHash)).to.be.false
     })
 
+    it('should register accountant', async () => {
+        // Topup some tokens into txMaker address so it could register accountant
+        await topUpTokens(token, txMaker, 10)
+        await token.approve(registry.address, 10)
+
+        await registry.registerAccountant(accountantOperator, 10)
+        accountantId = await registry.getAccountantAddress(accountantOperator)
+        expect(await registry.isActiveAccountant(accountantId)).to.be.true
+    })
+
     it('should register identity which has coins', async () => {
         const userAccount = otherAccounts[0]
         const registratinoFee = 100
+        const balanceBefore = Number(await token.balanceOf(registry.address))
 
         // Mint tokens into account
         const tokensToMint = 1000000
@@ -52,10 +67,10 @@ contract('Paid identity registry', ([owner, ...otherAccounts]) => {
         await token.approve(registry.address, registratinoFee, {from: userAccount})
 
         // Register account
-        await registry.registerIdentity(identityHash, {from: userAccount})
+        await registry.registerIdentity(identityHash, accountantId, 0, fundsDestination, {from: userAccount})
         expect(await registry.isRegistered(identityHash)).to.be.true
         
         // Registry should own some tokens
-        expect(Number(await token.balanceOf(registry.address))).to.be.equal(registratinoFee)
+        expect(Number(await token.balanceOf(registry.address))).to.be.equal(balanceBefore + registratinoFee)
     })
 })
