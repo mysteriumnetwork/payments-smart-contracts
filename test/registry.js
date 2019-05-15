56b475da3f00ef7614ccf065eba7331b7e6f1e9c
Jaro <jaro@nous.lt>
2019-05-15 14:28:46 +0300

    Merged registry tests into single file

--- test/registry.js
+++ test/registry.js
@@ -13,6 +13,7 @@ const MystDex = artifacts.require("MystDEX")
 
 const OneEther = OneToken = web3.utils.toWei('1', 'ether')
 const identityHash = '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+const secondIdentityHash = '0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
 
 contract('Registry', ([txMaker, minter, accountantOperator, fundsDestination, ...otherAccounts]) => {
     let token, channelImplementation, accountantId, dex, registry
@@ -73,5 +74,43 @@ contract('Registry', ([txMaker, minter, accountantOperator, fundsDestination, ..
             value: OneEther,
             gas: 200000
         }).should.be.rejected
+    })
+
+    // ==================== Paid registration ======================
+
+    it('should allow to change fee', async () => {
+        const newFee = 100
+        await registry.changeRegistrationFee(newFee)
+        const registrationFee = await registry.registrationFee()
+        expect(Number(registrationFee)).to.be.equal(newFee)
+    })
+
+    it('should fail registering identity having 0 balance', async () => {
+        const userAccount = otherAccounts[0]
+        expect(Number(await token.balanceOf(userAccount))).to.be.equal(0)
+
+        await registry.registerIdentity(secondIdentityHash, {from: userAccount}).should.be.rejected
+        expect(await registry.isRegistered(secondIdentityHash)).to.be.false
+    })
+
+    it('should register identity which has coins', async () => {
+        const userAccount = otherAccounts[0]
+        const registratinoFee = 100
+        const balanceBefore = Number(await token.balanceOf(registry.address))
+
+        // Mint tokens into account
+        const tokensToMint = 1000000
+        await token.mint(userAccount, tokensToMint)
+        expect(Number(await token.balanceOf(userAccount))).to.be.equal(tokensToMint)
+
+        // Approve registry to use tokens
+        await token.approve(registry.address, registratinoFee, {from: userAccount})
+
+        // Register account
+        await registry.registerIdentity(secondIdentityHash, accountantId, 0, fundsDestination, {from: userAccount})
+        expect(await registry.isRegistered(secondIdentityHash)).to.be.true
+        
+        // Registry should own some tokens
+        expect(Number(await token.balanceOf(registry.address))).to.be.equal(balanceBefore + registratinoFee)
     })
 })
