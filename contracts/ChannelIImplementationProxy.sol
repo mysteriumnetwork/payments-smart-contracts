ac0de8acc906e4a3fcf6e51fc99c17503a07ce0c
Jaro <jaro@nous.lt>
2020-05-01 11:04:43 +0300

    Config SC with different address on linuc

--- contracts/ChannelIImplementationProxy.sol
+++ contracts/ChannelIImplementationProxy.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.12 <0.6.0;
 import "./Config.sol";
 
 contract ChannelImplementationProxy {
-    address constant CONFIG_ADDRESS = 0x928b922896c0b4E33DD38dB721bAb6656836f52c;
+    address constant CONFIG_ADDRESS = 0x46e9742C098267122DA466d6b7a3fb844436Ac37;
 
     function () external payable {
         address _target = Config(CONFIG_ADDRESS).getAddress(0x48df65c92c1c0e8e19a219c69bfeb4cf7c1c123e0c266d555abb508d37c6d96e); // keccak256('channel implementation')
