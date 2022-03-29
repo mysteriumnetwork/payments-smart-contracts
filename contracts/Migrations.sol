39d83ce901263e0be631d52000e26196a75c1b55
Jaro <jaro@nous.lt>
2022-03-29 17:20:30 +0300

    Lock compiler version

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0;
+pragma solidity 0.8.9;
 
 contract Migrations {
   address public owner;
--- contracts/interfaces/IHermesContract.sol
+++ contracts/interfaces/IHermesContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.6;
+pragma solidity 0.8.9;
 
 interface IHermesContract {
     enum Status { Active, Paused, Punishment, Closed }
